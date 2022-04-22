f2811b98d7f98dca03962335a2f20733333271b2
ecetabag <49860124+ecetabag@users.noreply.github.com>
2022-04-22 11:46:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,27 @@ $('.navbar-toggle').click(function(){
     $(this).addClass('collapsed');
   }
  })
- 
+myFunction(){
+ var audionameslist = `
+        audiolib/1.mp3,
+        audiolib/2.mp3,
+        audiolib/3.mp3
+    `;
+    var audionamesarray = audionameslist.split(',');
+    var audio = new Audio(audionamesarray[0]);
+        
+    audio.src=audionamesarray[0];
+    audio.play();
+       
+    index=1;
+    audio.onended = function() {
+        if(index < audionamesarray.length){
+            audio.src=audionamesarray[index];
+            audio.play();
+            index++;
+        }
+    };
+}
  
 // Back to top Arrow
  
